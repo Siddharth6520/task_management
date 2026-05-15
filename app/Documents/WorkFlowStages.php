79a79396e06ad0580f861a4ae1a12d411350a333
Siddharth Krishnamoorthy <siddharth.k@myambergroup.com>
2026-05-15 12:33:11 +0530

    changes on task controller

--- app/Documents/WorkFlowStages.php
+++ app/Documents/WorkFlowStages.php
@@ -7,7 +7,7 @@ use App\Documents\Role;
 use App\Documents\User;
 use App\Documents\Department;
 use App\Documents\WorkTemplate;
-use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Doctrine\ODM\MongoDB\Mapping\Attribute as ODM;
 
 #[ODM\Document(collection: "workflow_stages")]
 #[ODM\Index(keys: [
@@ -37,7 +37,7 @@ class WorkStage
     | Workflow Template
     |--------------------------------------------------------------------------
     */
-
+//template-web id-1 ui-1->backend-2->frontend-3->qa-4->delivery-5
     #[ODM\ReferenceOne(
         targetDocument: WorkTemplate::class,
         storeAs: 'id'
@@ -79,7 +79,8 @@ class WorkStage
         return $this;
     }
 
-
+//template :- name:-web flow, code:- WEB, description:- when doing web developmnet
+//stage:- ui-1->backend-2->frontend-3->qa-4->delivery-5
 
     #[ODM\Field(type: "int")]
     private int $stage_order;
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\TeamMemberController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 
-Route::get('/users', [UserController::class, 'index'])
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+
+Route::get('/team_members', [TeamMemberController::class, 'index']);
 ?>
